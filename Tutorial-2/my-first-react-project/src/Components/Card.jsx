6c32609ba763a35b29af2cbff56fad3db28cb4b6
David MacLeod Mayes <d.macleodmayes@gmail.com>
2025-09-20 19:58:06 -0400

    adding a new world

---
+++ Tutorial-2/my-first-react-project/src/Components/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card(props) {
+    return (
+    <div className="Card-Component">
+        <img src={props.image} alt="" width="100"/>
+        <h2>{props.name}</h2>
+        <p>{props.price}</p>
+    </div>
+    );
+}
